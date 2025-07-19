4e0727e26ee33f4e504ea16a16e36f7af5fcfbe5
Keethers <keithsampl20@gmail.com>
2025-07-19 15:58:52 -0700

    Create index.js

---
+++ components/pages/index.js
@@ -0,0 +1,11 @@
+import ExoticaAI from "../components/ExoticaAI";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎰 Welcome to ExoticaEco$ystem 🎰</h1>
+      <p>Ready to launch your casino, trading AI, and token empire?</p>
+      <ExoticaAI />
+    </div>
+  );
+}

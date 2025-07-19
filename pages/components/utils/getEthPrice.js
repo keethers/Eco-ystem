a4446bc25181197f27b02f42140ef1f6b72e39b9
Keethers <keithsampl20@gmail.com>
2025-07-19 16:19:05 -0700

    Create getEthPrice.js

---
+++ pages/components/utils/getEthPrice.js
@@ -0,0 +1,5 @@
+export async function getEthPrice() {
+  const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
+  const data = await res.json();
+  return data.ethereum.usd;
+}

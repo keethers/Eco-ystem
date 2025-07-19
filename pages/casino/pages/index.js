943f3f0f9f104a3a42c09aea5499c7252d02a03e
Keethers <keithsampl20@gmail.com>
2025-07-19 16:55:08 -0700

    Create index.js

---
+++ pages/casino/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExoNavBar from '../components/ExoNavBar';
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <ExoNavBar />
+      <h1>🚀 Welcome to ExoticaEco$ystem</h1>
+      <p>Play games. Win EX$. Live the dream.</p>
+    </div>
+  );
+}

aefc98fef0895ad9c37ff42f83b2de33287839f7
Keethers <keithsampl20@gmail.com>
2025-07-19 17:30:05 -0700

    Create jackpot.js

---
+++ components/pages/casino/jackpot.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExoNavBar from "../../components/ExoNavBar";
+
+export default function JackpotPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <ExoNavBar />
+      <h1>💰 Jackpot Coming Soon...</h1>
+      <p>Stay tuned for the ultimate EX$ prize pool drop!</p>
+    </div>
+  );
+}

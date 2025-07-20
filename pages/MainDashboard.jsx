32a4163545a0c378a4b189014fdc125d25f32856
Keethers <keithsampl20@gmail.com>
2025-07-19 20:04:07 -0700

    Create MainDashboard.jsx

---
+++ pages/MainDashboard.jsx
@@ -0,0 +1,6 @@
+const socialLinks = [
+  { icon: '📘', name: 'Facebook', url: 'https://facebook.com' },
+  { icon: '🎵', name: 'TikTok', url: 'https://tiktok.com' },
+  ...
+];
+{ icon: '🎧', name: 'Spotify', url: 'https://spotify.com' },
